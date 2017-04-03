240e3206c56868f85604afd1ca19df72490b0c93
kuangmin <qiangminmin520@gmail.com>
2017-04-03 22:03:32 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,6 +7,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
 def show
   @group =Group.find(params[:id])
+  @posts = @group.posts
 end
 
 def edit
@@ -58,7 +59,7 @@ def find_group_and_check_permission
   if current_user != @group.user
     redirect_to root_path, alert: "You have no permission."
   end
-end 
+end
 
 def group_params
   params.require(:group).permit(:title, :description)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belonss_to :group
+  belongs_to :group
+  validates :content, presence: true
 end
